60113b3e86fafe835cb480d87a5640c246be4f4a
Thiago Domingos <99504975+ThiagoDomingoss@users.noreply.github.com>
2023-01-17 18:16:38 -0300

    Iniciando projeto de ponta a ponta

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import Habit from "./components/Habit"
+import './styles/global.css';
+
+function App() {
+  
+
+  return (
+    <div>
+      <Habit completed={5}/>
+      <Habit completed={7}/>
+      <Habit completed={2}/>
+      <Habit completed={6}/>
+    </div>
+  )
+}
+
+export default App
